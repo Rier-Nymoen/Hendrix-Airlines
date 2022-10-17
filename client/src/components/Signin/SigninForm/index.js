242a10908fe47d41d3ecbf6d63d5225f701e28e2
Jimmy <jimmyrod264@gmail.com>
2022-10-16 22:59:02 -0400

    added commented calendar code

--- client/src/components/Signin/SigninForm/index.js
+++ client/src/components/Signin/SigninForm/index.js
@@ -1,33 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {signinSchema} from "./validationSchema";
 import {Field, Formik} from "formik";
 import TextField from '@mui/material/TextField';
 import {SigninButton} from "../SigninElements";
-import {FormikForm, initialValues, onSubmit} from "./FormElements";
+import {FormikForm, initialValues} from "./FormElements"; //onSubmit
+import axios from "axios";
 
 
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 // import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 //
-// function BasicDatePicker() {
-//   const [value, setValue] = React.useState(null);
-//
+// const BasicDatePicker = () => {
+//   const [date, setDate] = useState(null);
+//   const [error, setError] = useState(false);
+//   // if (date) {
+//   //     console.log(date.toLocaleString());
+//   // }
 //   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//     <LocalizationProvider dateAdapter={AdapterDateFns}>
 //       <DatePicker
 //         label="Departure Date"
-//         value={value}
-//         onChange={(newValue) => {
-//           setValue(newValue);
+//         value={date}
+//         disablePast={true}
+//         onError={(reason) => {
+//             if (reason) {
+//                 setError(true)
+//             }
+//             else {
+//                 setError(false)
+//             }
 //         }}
-//         renderInput={(params) => <TextField {...params} />}
+//         onChange={(newDate) => {
+//           setDate(newDate);
+//         }}
+//         renderInput={(params) => <TextField{...params}
+//                                            helperText={error ? "Invalid Date" : " "}/>}
 //       />
 //     </LocalizationProvider>
 //   );
 // }
 
 const SigninForm = () => {
+    const onSubmit = async (signinData, { setSubmitting, setFieldError }) => {
+        setSubmitting(true);
+        try {
+            const response = await axios.get('http://localhost:5000/users/' + signinData.email);
+
+            if (response.status !== 200) {
+                alert('API Status Error: ' + response.status);
+            } else if (response.data.length === 0) {
+                setFieldError('email', 'Email not found');
+            } else if (response.data[0].password !== signinData.password) {
+                setFieldError('password', 'Incorrect password');
+            } else {
+                alert('Logged in');
+            }
+        }
+        catch (error) {
+            alert(error);
+        }
+
+        setSubmitting(false);
+    };
+
     return (
         <Formik
             validateOnChange={true}
@@ -54,6 +90,7 @@ const SigninForm = () => {
                 {/*<pre>{JSON.stringify(values, null, 2)}</pre>*/}
                 {/*<pre>{JSON.stringify(errors, null, 2)}</pre>*/}
                 <SigninButton disabled={isSubmitting} type="submit">Sign In</SigninButton>
+                {/*<BasicDatePicker />*/}
             </FormikForm>
             )}
         </Formik>
